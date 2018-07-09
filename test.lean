c69d9943d6eea2b5d869d1a7331bf8aeeb06de09
CoselTech <coseltech@pareidolia.nl>
2018-07-10 00:50:54 +0200

    tptp emitter

--- test.lean
+++ test.lean
@@ -13,7 +13,9 @@ meta inductive folterm
 | app : folterm → folterm → folterm
 
 meta inductive folform 
-| app : folpred → list folterm → folform
+| P : folterm → folform
+| T : folterm → folterm → folform
+| eq : folterm → folterm → folform
 | bottom : folform
 | top : folform
 | neg : folform → folform
@@ -23,6 +25,135 @@ meta inductive folform
 | disj : folform → folform → folform
 | all : name → name → folform → folform
 | exist : name → name → folform → folform
+
+meta structure introduced_constant :=
+(n : name) (e : expr) -- name and type
+
+meta structure axioma :=
+(n : name) (f : folform)
+
+meta structure hammer_state :=
+(axiomas : list axioma)
+(introduced_constants : list introduced_constant)
+
+meta def hammer_tactic :=
+state_t hammer_state tactic
+
+section tptp
+open format
+
+-- axiom is a reserved word in Lean
+meta inductive role
+| axioma : role
+| conjecture : role
+
+meta def role.to_format : role → format
+| role.axioma := to_fmt "axiom"
+| role.conjecture := to_fmt "conjecture"
+
+meta instance : has_to_format role :=
+⟨role.to_format⟩
+
+meta def folform.to_format_collect_vars : folform → list name → (list name × folform) 
+| (folform.all n n1 e@(folform.all _ _ _)) xs := folform.to_format_collect_vars e (n :: xs)
+| (folform.exist n n1 e@(folform.exist _ _ _)) xs := folform.to_format_collect_vars e (n :: xs)
+| (folform.all n n1 e) xs := (n :: xs, e)
+| (folform.exist n n1 e) xs := (n :: xs, e)
+| e@_ xs := (xs, e)
+
+meta def name_to_id_format : name → format
+| n := "'" ++ to_fmt n ++ "'"
+
+meta def names_to_id_format : list name → nat → list format
+| (x::xs) d := ("V" ++ to_fmt (d+1) ++ " /* " ++ to_fmt x ++ " */") :: names_to_id_format xs (d+1)
+| [] d := []
+
+meta def mygroup : format → format := id
+
+
+meta def folterm.to_format : folterm → nat → format
+| (folterm.const n) _ := "'" ++ to_fmt n ++ "'"
+| (folterm.lconst n _) _ := "'" ++ to_fmt n ++ "'"
+| (folterm.prf) _ := "prf"
+| (folterm.var i) depth := "V" ++ to_fmt (depth - i)
+| (folterm.app t u) depth :=
+  "a(" ++ t.to_format depth ++
+    (mygroup $ nest 2 $ "," ++ line ++ u.to_format depth)
+
+
+meta def folform.to_format_aux : folform → nat → format
+| e@(folform.all n n1 f) depth :=
+  let (ys, g) := folform.to_format_collect_vars e [] in
+  let ys' := names_to_id_format ys depth in
+    "! [" ++ 
+    (mygroup $ nest 3 $ (join $ list.intersperse ("," ++ line) ys') ++ "] :" ++ line) ++
+    "(" ++ (mygroup $ nest 1 $ g.to_format_aux $ depth + ys.length) ++ ")"
+| e@(folform.exist n n1 f) depth :=
+  let (ys, g) := folform.to_format_collect_vars e [] in
+  let ys' := names_to_id_format ys depth in
+    "? [" ++ 
+    (mygroup $ nest 3 $ (join $ list.intersperse ("," ++ line) ys') ++ "] :" ++ line) ++
+    "(" ++ (mygroup $ nest 1 $ g.to_format_aux $ depth + ys.length) ++ ")"
+| folform.bottom _ := to_fmt "$false"
+| folform.top _ := to_fmt "$true"
+| (folform.P t) depth :=
+  "p(" ++ (nest 2 $ t.to_format depth) ++ ")"
+| (folform.T t u) depth :=
+  "t(" ++ (mygroup $ nest 2 $ t.to_format depth ++ "," ++ line ++ u.to_format depth ++ ")")
+| (folform.eq t u) depth :=
+  "(" ++ (nest 1 $ t.to_format depth) ++
+    (mygroup $ line ++ "= " ++ (nest 2 $ u.to_format depth ++ ")"))
+| (folform.neg f) depth := 
+  match f with
+  | (folform.eq t u) :=
+    "(" ++ (nest 1 $ t.to_format depth) ++
+      (mygroup $ line ++ "!= " ++ (nest 3 $ u.to_format depth ++ ")"))
+  | _ := "~(" ++ (nest 2 $ f.to_format_aux depth ++ ")")
+  end
+| (folform.imp f g) depth :=
+  "(" ++ (nest 1 $ f.to_format_aux depth ++ ")") ++ 
+    (mygroup $ line ++ (nest 4 $ "=> (" ++ g.to_format_aux depth ++ ")"))
+| (folform.iff f g) depth :=
+  "(" ++ (nest 1 $ f.to_format_aux depth ++ ")") ++
+    (mygroup $ line ++ (nest 5 $ "<=> (" ++ g.to_format_aux depth ++ ")"))
+| (folform.conj f g) depth :=
+  "(" ++ (nest 1 $ f.to_format_aux depth ++ ")") ++
+    (mygroup $ line ++ (nest 5 $ "& (" ++ g.to_format_aux depth ++ ")"))
+| (folform.disj f g) depth :=
+  "(" ++ (nest 1 $ f.to_format_aux depth ++ ")") ++
+    (mygroup $ line ++ (nest 5 $ "| (" ++ g.to_format_aux depth ++ ")"))
+
+      
+
+meta def folform.to_format (f : folform) : format := folform.to_format_aux f 0
+
+meta instance : has_to_format folform :=
+⟨folform.to_format⟩
+
+meta def to_fof : string → role → folform → format
+| id r f :=
+to_fmt "fof("
+++ (mygroup $ nest 4 $ join $ list.intersperse ("," ++ line) $
+  [to_fmt id, to_fmt r, "(" ++ to_fmt f ++ ")"]) ++ to_fmt ")."
+  
+meta def to_tptp : list axioma → folform → format
+| (⟨n, f⟩::as) conjecture := 
+  to_fof ("'" ++ to_string n ++ "'") role.axioma f
+     ++ line
+     ++ line
+     ++ to_tptp as conjecture
+| [] conjecture := to_fof "'problem_conjecture'" role.conjecture conjecture
+
+
+meta def myformat {α : Type} [has_to_format α] : list α → format
+| [] := to_fmt "[]"
+| xs := to_fmt "[" ++ mygroup (format.nest 1 $ format.join $ list.intersperse ("," ++ format.line) $ xs.map to_fmt) ++ to_fmt "]"
+
+end tptp 
+
+
+
+
 
 meta def name.repr : name → string
 | name.anonymous := "name.anonymous"
@@ -51,7 +182,9 @@ meta instance : has_repr folpred :=
 ⟨folpred.repr⟩
 
 meta def folform.repr : folform → string
-| (folform.app p ts) := "(folform.app " ++ repr p ++  " " ++ repr ts ++ ")"
+| (folform.P t) := "(folform.P " ++ repr t ++ ")"
+| (folform.T t u) := "(folform.T " ++ repr t ++  " " ++ repr u ++ ")"
+| (folform.eq t u) := "(folform.eq " ++ repr t ++  " " ++ repr u ++ ")"
 | folform.bottom := "folform.bottom"
 | folform.top := "folform.top"
 | (folform.neg f) := "(folform.neg " ++ folform.repr f ++ ")"
@@ -66,15 +199,25 @@ meta instance : has_repr folform :=
 ⟨folform.repr⟩
 
 
-meta structure introduced_constant :=
-(n : name) (e : expr)
 
-meta structure hammer_state :=
-(axiomas : list folform)
-(introduced_constants : list introduced_constant)
 
-meta def hammer_tactic :=
-state_t hammer_state tactic
+meta def all_created_equal : folform :=
+folform.all `h1 `h1 $ folform.all `h2 `h2 $
+  folform.imp
+    (folform.conj (folform.P $ folterm.var 0) (folform.P $ folterm.var 1))
+    (folform.T (folterm.var 0) (folterm.var 1))
+
+
+set_option pp.indent 2
+set_option pp.colors true
+set_option pp.width 1
+
+
+
+#eval format.to_string $ to_fof "all_created_equal" role.axioma all_created_equal   
+
+
+
 
 meta instance : monad hammer_tactic :=
 state_t.monad 
@@ -94,18 +237,11 @@ meta def lives_in_prop_p (e : expr) : hammer_tactic bool :=
 do tp ← tactic.infer_type e,
    return (if eq tp (expr.sort level.zero) then tt else ff)
 
-/-
-meta def fresh_folvar : hammer_tactic folvar :=
-do ⟨idx⟩ ← state_t.get,
-   state_t.modify (fun state, {state with nextvaridx := state.nextvaridx + 1}),
-   return (folvar.var2 idx)
-   -/
-
-meta def add_axiom (axioma : folform) : hammer_tactic unit :=
-do state_t.modify (fun state, {state with axiomas := axioma :: state.axiomas})
+meta def add_axiom (n : name) (axioma : folform) : hammer_tactic unit :=
+do state_t.modify (fun state, {state with axiomas := ⟨n, axioma⟩ :: state.axiomas})
 
 meta def add_constant (n : name) (e : expr) : hammer_tactic unit :=
-do state_t.modify (fun state, {state with introduced_constants := introduced_constant.mk n e :: state.introduced_constants })
+do state_t.modify (fun state, {state with introduced_constants := ⟨n, e⟩ :: state.introduced_constants })
 
 meta def doforget {α : Type} (t₁ : tactic α)  : tactic α :=
 λ s, interaction_monad.result.cases_on (t₁ s)
@@ -118,8 +254,6 @@ state_t.mk (λ x, do ⟨a, b⟩ ← doforget (t₁.run x),
 
 -- might want to do something different
 meta def mk_fresh_name : tactic name := tactic.mk_fresh_name
-
-meta def mk_fresh_constant : hammer_tactic folterm := do n ← mk_fresh_name, return $ folterm.const n
 
 meta def body_of : expr → hammer_tactic (expr × name)
 | e@(expr.pi n bi d b) := do id ← mk_fresh_name,
@@ -142,7 +276,9 @@ meta def folterm.abstract_locals_core : folterm → ℕ → list name → folter
 meta def folterm.abstract_locals : folterm → list name → folterm := λ f l, f.abstract_locals_core 0 l
 
 meta def folform.abstract_locals_core : folform → nat → list name → folform
-| e@(folform.app p ts) d ln := folform.app p $ ts.map (λ t, t.abstract_locals_core d ln)
+| e@(folform.P t) d ln := folform.P $ t.abstract_locals_core d ln
+| e@(folform.T t u) d ln := folform.T (t.abstract_locals_core d ln) (u.abstract_locals_core d ln)
+| e@(folform.eq t u) d ln := folform.eq (t.abstract_locals_core d ln) (u.abstract_locals_core d ln)
 | e@(folform.neg f) d ln := f.abstract_locals_core d ln
 | e@(folform.imp f1 f2) d ln := folform.imp (f1.abstract_locals_core d ln) (f2.abstract_locals_core d ln)
 | e@(folform.iff f1 f2) d ln := folform.iff (f1.abstract_locals_core d ln) (f2.abstract_locals_core d ln)
@@ -212,27 +348,30 @@ with hammer_c : expr → hammer_tactic folterm
       end
 | e@(expr.pi n _ t _) :=
   do  lip ← lives_in_prop_p e,
-      F ← mk_fresh_constant,
+      Fn ← mk_fresh_name,
+      let F := folterm.const Fn,
+      let An := n ++ Fn,
       ys ← hammer_ff $ hammer_fc e,
       let ce0 := list.foldl (λ t (np : name × name), folterm.app t (folterm.lconst np.1 np.2)) F ys,
       if lip
       then
-        (do let ce1 := folform.app folpred.P [ce0], 
+        (do let ce1 := folform.P ce0, 
             ce2 ← hammer_f e,
-            add_axiom $ wrap_quantifier folform.all ys (folform.iff ce1 ce2))
+            add_axiom An $ wrap_quantifier folform.all ys (folform.iff ce1 ce2))
       else
         (do zn ← mk_fresh_name,
             let zlc := folterm.lconst zn zn, 
             let ys := (⟨zn, zn⟩ :: ys : list $ name × name),
-            let ce1 := folform.app folpred.T [zlc, ce0],
+            let ce1 := folform.T zlc ce0,
             ce2 ← hammer_g zlc e,
-            add_axiom $ wrap_quantifier folform.all ys (folform.iff ce1 ce2)),
+            add_axiom An $ wrap_quantifier folform.all ys (folform.iff ce1 ce2)),
       return ce0
-| e@(expr.lam _ _ _ _) :=
+| e@(expr.lam n _ _ _) :=
   do  (t, xτs) ← collect_lambdas e, 
       α ← tactic.infer_type t,
       let yρs := hammer_fc e,
       Fn ← mk_fresh_name,
+      let An := n ++ Fn,
       y₀s ← hammer_ff yρs,
       x₀s ← hammer_ff xτs,
       let Ft :=
@@ -257,7 +396,7 @@ with hammer_c : expr → hammer_tactic folterm
       -- ce1b ← tactic.to_expr ``(eq %%ce1a %%t), 
       -- tactic.to_expr is going to blow up if operands are not of the same type. Exciting.
       ce1 ← hammer_f ce1b',
-      add_axiom $ wrap_quantifier folform.all (y₀s ++ x₀s) ce1,
+      add_axiom An $ wrap_quantifier folform.all (y₀s ++ x₀s) ce1,
       return $
         list.foldl
           (λ a (nt : name × name), folterm.app a $ folterm.lconst nt.1 nt.2)
@@ -267,6 +406,7 @@ with hammer_c : expr → hammer_tactic folterm
   do  let yαs := hammer_fc (t τ),  
       y₀s ← hammer_ff yαs,
       Fn ← mk_fresh_name,
+      let An := x ++ Fn,
       let Ft :=
         list.foldr
           (λ (nt : name × name × expr) a,
@@ -294,10 +434,10 @@ with hammer_c : expr → hammer_tactic folterm
       if lip
       then
         do  ce2 ← hammer_c t,
-            add_axiom $ wrap_quantifier folform.all y₀s $
-              folform.app folpred.eq [
-                (list.foldl (λ e (nt : name × name), (folterm.app e (folterm.const nt.1))) (folterm.const Fn) y₀s),
-                ce2]
+            add_axiom An $ wrap_quantifier folform.all y₀s $
+              folform.eq 
+                (list.foldl (λ e (nt : name × name), (folterm.app e (folterm.const nt.1))) (folterm.const Fn) y₀s)
+                ce2
       else
         return (),
       hammer_c $ expr.instantiate_var s ce1
@@ -323,7 +463,7 @@ with hammer_g : folterm → expr → hammer_tactic folform
             return $ wrap_quantifier folform.all [(xn, n)] (folform.imp ge1 ge2)
 | u w :=
   do  ge1 ← hammer_c w,
-      return $ folform.app folpred.T [u, ge1]
+      return $ folform.T u ge1
 
 
 with hammer_f : expr → hammer_tactic folform 
@@ -365,8 +505,11 @@ with hammer_f : expr → hammer_tactic folform
 | `(%%t = %%s) :=
   do  fe1 ← hammer_c t,
       fe2 ← hammer_c s,
-      return $ folform.app folpred.eq [fe1, fe2]                                                     
+      return $ folform.eq fe1 fe2                                                    
 | t  :=
   do  ge1 ← hammer_c t,
-      return $ folform.app folpred.P [ge1]
+      return $ folform.P ge1
 
+-- TODO negation, false? Map to bottom etc.
+-- eprover, vampire
+-- TODO third phase section 5 Translation?
